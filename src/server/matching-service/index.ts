b75870f1bd37a5bcba6662c85d0ccf0bc4fc2a8b
Earleking <arek.fielding@gmail.com>
2018-11-08 13:32:00 -0500

    compatibility changes

--- src/server/matching-service/index.ts
+++ src/server/matching-service/index.ts
@@ -4,11 +4,33 @@ class matchFinder {
 
     }
     findMatches(players:any) {
+        var curPlayers:any = [];
         for(var i in players) {
-            var player:any = players[i];
+            curPlayers = [];
+            curPlayers.push(players[i]);
             var classes:any;
-            var bestClass = getLargestKey(rules["rules"]["compPenalty"][classes[0]]);
+            for(var z in players) {
+                calculateScore([...curPlayers, players[z]]);
+            }
             
+        }
+    }
+}
+
+function calculateScore(players:any) {
+    var curScore:Number = 100;
+    // players is an array of the players json.
+    for(var i in players) {
+        for(var t in players) {
+            // If players are the same skip
+            if(t == i) {
+                continue;
+            }
+
+            // Compare scores for classes
+            for(var typeI in players[i]["classes"])  {
+
+            }
         }
     }
 }
@@ -17,3 +39,4 @@ function getLargestKey(dict: any) {
     return Object.keys(dict).reduce(function(a, b){ return dict[a] > dict[b] ? a : b });
 }
 
+// function getBest
