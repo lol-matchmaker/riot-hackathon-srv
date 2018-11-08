4aba87daf4851e20df8af1b44cf6b369e2c658b9
ey1031 <eyudeveloper@gmail.com>
2018-11-08 13:35:44 -0800

    aggressive match

--- src/server/matching-service/aggressive.ts
+++ src/server/matching-service/aggressive.ts
@@ -1,11 +1,13 @@
 import request = require('request-promise-native');
+const secret: { api_key: string } = require('../../../src/fetcher/secret.js');
+
 
 // Extract user's participantId out of match by summonerName
-const findParticipantId = async(match_id: string) => {
+const findParticipantId = async(match_id: number, summonerName: string) => {
     const matchJson = await request({
-        headers: {'X-Riot-Token': 'RGAPI-5d326d57-5f0d-4c19-a377-fc020817d993'},
+        headers: {'X-Riot-Token': secret.api_key},
         json: true,
-        url: `https://na1.api.riotgames.com/lol/match/v4/matches/2904607483`,
+        url: `https://na1.api.riotgames.com/lol/match/v4/matches/${match_id}`,
     });
 
     let data = {
@@ -13,7 +15,7 @@ const findParticipantId = async(match_id: string) => {
         participantId: 0
     }
     for (let participant of matchJson.participantIdentities) {
-        if (participant.player.summonerName === 'Vauss') {
+        if (participant.player.summonerName === summonerName) {
             data.participantId = participant.participantId
         }
     }
@@ -25,31 +27,54 @@ const findParticipantId = async(match_id: string) => {
     return data
 }
 
-// returns true if aggressive kill, else false
-const isAggressiveKill = async () => {
-    let data = await findParticipantId('2904607483')
+// Returns true if aggressive kill count % is greater than 50% in a game, else false
+const isAggressiveMatch = async (match_id: number, summonerName: string) => {
+    let data = await findParticipantId(match_id, summonerName)
+    let kill_count = 0
+    let aggressive_kill_count = 0
     const mapJson = await request({
-        headers: {'X-Riot-Token': 'RGAPI-5d326d57-5f0d-4c19-a377-fc020817d993'},
+        headers: {'X-Riot-Token': secret.api_key},
         json: true,
-        url: `https://na1.api.riotgames.com/lol/match/v4/timelines/by-match/2904607483`,
+        url: `https://na1.api.riotgames.com/lol/match/v4/timelines/by-match/${match_id}`,
     });
     for (let frame of mapJson.frames) {
         for (let event of frame.events) {
-            if (event.type === 'CHAMPION_KILL' && event.killerId == data.participantId ){
-                let y_predicted = -0.9733 * event.position.x + 14629
-                if (y_predicted > event.position.y && data.teamId === 200 ||
-                    y_predicted < event.position.y && data.teamId === 100) {
-                    // blue side kill, team is red
-                    // red side kill, team is blue
-                    return true
+            if (event.type === 'CHAMPION_KILL') {
+                if (event.killerId == data.participantId ){
+                    kill_count += 1
+                    let y_predicted = -0.9733 * event.position.x + 14629
+                    if (y_predicted > event.position.y && data.teamId === 200 ||
+                        y_predicted < event.position.y && data.teamId === 100) {
+                        // blue side kill, team is red
+                        // red side kill, team is blue
+                        aggressive_kill_count += 1
+                    }
                 }
             }
         }
     }
+    if (aggressive_kill_count / kill_count > 0.5) {
+        return true
+    }
     return false
 }
 
-isAggressiveKill().then(function(res) {
+const isAggressivePlayer = async(accountId: number) => {
+    const matchJson = await request({
+        headers: {'X-Riot-Token': secret.api_key},
+        json: true,
+        url: `https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/${accountId}`,
+    });
+
+    for (let match of matchJson.participantIdentities) {
+        console.log(match)
+    }
+}
+
+isAggressivePlayer(51345606).then(function(res) {
+    console.log(res)
+})
+isAggressiveMatch(2904607483, 'Vauss').then(function(res) {
     console.log(res)
 })
 
