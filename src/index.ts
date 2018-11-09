0fe1f4df013a1753730de5cb36b9ea6967575c34
Victor Costan <costan@gmail.com>
2018-11-08 20:11:24 -0800

    Some infra for matches.

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import { app } from './api/app';
 import { sequelize } from './db/connection';
 import { Multiplexer } from './queue/multiplexer';
 import { SwitchBox } from './queue/switch_box';
+import { setAppStatusSwitchBox } from './api/status';
 
 const main = async () => {
   await sequelize.sync();
 
   const switchBox = new SwitchBox();
+  setAppStatusSwitchBox(switchBox);
   const multiplexer = new Multiplexer(app,
                                       switchBox,
                                       parseInt(process.env['PORT'] || '3000'));
